608d16580908e33f6f71575bc9160b65622cd329
OoperLoops <ooperloops@gmail.com>
2025-10-03 14:44:04 -0600

    Added basic file hierarchy

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/_pedroPathing/Tuning.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/_pedroPathing/Tuning.java
@@ -1,11 +1,11 @@
-package org.firstinspires.ftc.teamcode.pedroPathing;
+package org.firstinspires.ftc.teamcode._pedroPathing;
 
-import static org.firstinspires.ftc.teamcode.pedroPathing.Tuning.changes;
-import static org.firstinspires.ftc.teamcode.pedroPathing.Tuning.drawOnlyCurrent;
-import static org.firstinspires.ftc.teamcode.pedroPathing.Tuning.draw;
-import static org.firstinspires.ftc.teamcode.pedroPathing.Tuning.follower;
-import static org.firstinspires.ftc.teamcode.pedroPathing.Tuning.stopRobot;
-import static org.firstinspires.ftc.teamcode.pedroPathing.Tuning.telemetryM;
+import static org.firstinspires.ftc.teamcode._pedroPathing.Tuning.changes;
+import static org.firstinspires.ftc.teamcode._pedroPathing.Tuning.drawOnlyCurrent;
+import static org.firstinspires.ftc.teamcode._pedroPathing.Tuning.draw;
+import static org.firstinspires.ftc.teamcode._pedroPathing.Tuning.follower;
+import static org.firstinspires.ftc.teamcode._pedroPathing.Tuning.stopRobot;
+import static org.firstinspires.ftc.teamcode._pedroPathing.Tuning.telemetryM;
 
 import com.bylazar.configurables.PanelsConfigurables;
 import com.bylazar.configurables.annotations.Configurable;
